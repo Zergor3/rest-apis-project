d404193fdb5d95af60c7bad4d02657d765691b8a
Hector Gomez <hgl502@hotmail.com>
2023-04-11 15:16:37 -0500

    docs(app): modify app.

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
 
     migrate = Migrate(app, db)
     api = Api(app)
